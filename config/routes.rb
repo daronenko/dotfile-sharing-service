2dd735bf892d8bf350a211b109d35fcc6e25f42f
Daniil Mironenko <dan.mironenko@gmail.com>
2023-10-29 15:03:37 +0300

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
   resources :dotfiles
+
   root 'home#index'
   get 'home/about'
 end
