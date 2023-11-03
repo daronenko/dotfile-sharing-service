f72a86102cd89ef6d674af3ad9ca3cab49b63fbb
Daniil Mironenko <dan.mironenko@gmail.com>
2023-11-03 22:53:22 +0300

    add sign in with username or email

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,11 +38,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
+  validates :username, presence: true, uniqueness: true
 
   has_many :dotfiles
+
+  attr_accessor :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  private
+
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where([
+        "lower(username) = :value OR lower(email) = :value", { value: login.downcase }
+      ]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
