487c7e6b274b95c63305513dd7b3908b8917725a
Daniil Mironenko <dan.mironenko@gmail.com>
2023-12-09 20:03:00 +0300

    add filtering by config type

--- app/controllers/dotfiles_controller.rb
+++ app/controllers/dotfiles_controller.rb
@@ -7,11 +7,11 @@ class DotfilesController < ApplicationController
 
   # GET /dotfiles or /dotfiles.json
   def index
-    search_params = params.permit(:format, :page, q: [:title_or_description_or_config_type_or_user_username_cont])
+    search_params = params.permit(:format, :page, q: [:title_or_description_or_config_type_or_user_username_cont, :config_type_eq])
     @q = Dotfile.ransack(search_params[:q])
-    # dotfiles = @q.result(distinct: true).order(created_at: :asc)  # TODO:add sorting option 
+    # dotfiles = @q.result(distinct: true).order(created_at: :asc)  # TODO: add sorting option 
     dotfiles = @q.result(distinct: true).order(cached_weighted_like_score: :desc)
-    @pagy, @dotfiles = pagy(dotfiles, items: 10)
+    @pagy, @dotfiles = pagy(dotfiles, items: 1)
   rescue Pagy::OverflowError
     redirect_to dotfiles_path(page: 1)
   end
