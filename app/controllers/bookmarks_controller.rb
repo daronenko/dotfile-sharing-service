23382291847c1f487f780fef66b0276fab9d41e8
Daniil Mironenko <dan.mironenko@gmail.com>
2023-12-13 20:26:32 +0300

    fix incorrect bookmark search

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,8 +7,8 @@ class BookmarksController < ApplicationController
     search_params = params.permit(:format, :page, q: [:title_or_description_or_config_type_or_user_username_cont, :config_type_eq])
     @q = Dotfile.ransack(search_params[:q])
     dotfiles = @q.result(distinct: true).order(cached_weighted_like_score: :desc)
-    @pagy, @dotfiles = pagy(dotfiles, items: 10)
+    @bookmarks_pagy, @bookmarks = pagy(dotfiles, items: 2)
   rescue Pagy::OverflowError
-    redirect_to dotfiles_path(page: 1)
+    redirect_to bookmarks_index_path(page: 1)
   end
 end
