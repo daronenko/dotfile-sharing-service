0497dfa85a1ca2135877a08299b3839ed89cb2fe
Daniil Mironenko <dan.mironenko@gmail.com>
2023-12-17 14:15:10 +0300

    add validations

--- app/models/dotfile.rb
+++ app/models/dotfile.rb
@@ -1,6 +1,9 @@
 class Dotfile < ApplicationRecord
   has_one_attached :file
   belongs_to :user
+
+  validates :file, presence: true
+  validate :allowed_file_extension
   
   acts_as_votable
 
@@ -25,6 +28,13 @@ class Dotfile < ApplicationRecord
       unvote_by user, vote_scope: "like"
     else
       downvote_by user, vote_scope: "like"
+    end
+  end
+
+  def allowed_file_extension
+    allowed_extensions = %w[fish lua conf]
+    if file.present? && !allowed_extensions.include?(File.extname(file.filename.to_s).delete("."))
+      errors.add(:file, I18n.t('errors.invalid_extension'))
     end
   end
 
